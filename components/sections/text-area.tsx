6048bb07090aaa9b37294e2a8f704f765db37483
Boye Borg <boye.borg@icloud.com>
2020-04-13 17:22:21 +0200

    add styling for text area section

--- components/sections/text-area.tsx
+++ components/sections/text-area.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import BlockContentToReact from "@sanity/block-content-to-react";
 import { SanityTextArea } from "../../sanity/models";
+import { css } from "@emotion/core";
+
+const wrapper = css`
+	max-width: 1000px;
+	padding: 2rem;
+	margin: 0 auto;
+`;
 
 type Props = {
 	content: SanityTextArea;
 };
 
 const TextArea: React.FC<Props> = (props) => {
-	return <BlockContentToReact blocks={props.content.text} />;
+	return (
+		<article css={wrapper}>
+			<BlockContentToReact blocks={props.content.text} />
+		</article>
+	);
 };
 
 export default TextArea;
