0957d651359164b7b838b8b26bcd6726865fd9b0
Boye Borg <boye.borg@icloud.com>
2020-04-13 23:13:01 +0200

    style advertisement

---
+++ components/product-preview.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { css } from "@emotion/core";
+import { SanityProduct } from "../sanity/models";
+import { urlFor } from "../sanity";
+
+const articleWrapper = css`
+	display: flex;
+	flex-direction: column;
+	text-align: center;
+
+	& img {
+		width: 100%;
+		height: auto;
+	}
+
+	& > h2 {
+		margin: 0.25rem 0 0.125rem 0;
+		font-weight: normal;
+	}
+
+	& > h3 {
+		margin: 0;
+		font-size: 1rem;
+		font-weight: normal;
+		::after {
+			content: ",-";
+		}
+	}
+`;
+
+type props = {
+	product: SanityProduct;
+};
+
+const ProductPreview: React.FC<props> = (props) => {
+	const {
+		product: { defaultPrice, title, images = [] },
+	} = props;
+
+	const imageUrl = urlFor(images[0]).format("webp").maxWidth(500).url() || "";
+	const fallbackImageUrl = urlFor(images[0]).maxWidth(500).url() || "";
+
+	return (
+		<article css={articleWrapper}>
+			<picture>
+				<source srcSet={`${imageUrl} 1x`} type="image/webp" />
+				<img src={fallbackImageUrl} alt={images[0]?.alt} />
+			</picture>
+			<h2>{title}</h2>
+			<h3>{defaultPrice}</h3>
+		</article>
+	);
+};
+
+export default ProductPreview;
--- components/sections/advertisement.tsx
+++ components/sections/advertisement.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { SanityAdvertisement } from "../../sanity/models";
 import { css } from "@emotion/core";
+import ProductPreview from "../product-preview";
 
-const wrapper = css`
+const wrapper = (numProducts: number) => css`
 	max-width: 1000px;
 	padding: 2rem;
 	margin: 0 auto;
+
+	& > h1 {
+		text-align: center;
+	}
+
+	& > ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		display: grid;
+		justify-content: center;
+		grid-row-gap: 0.5rem;
+
+		@media screen and (min-width: 600px) {
+			grid-column-gap: 2rem;
+			grid-template-columns: repeat(6, 1fr);
+			grid-template-areas: ${numProducts === 1
+				? `". p1 p1 p1 p1 ."`
+				: numProducts === 2
+				? `"p1 p1 p1 p2 p2 p2"`
+				: `"p1 p1 p1 p1 p2 p2" "p1 p1 p1 p1 p3 p3"`};
+		}
+	}
 `;
 
 type Props = {
@@ -16,12 +40,19 @@ const Advertisement: React.FC<Props> = (props) => {
 	const { content } = props;
 
 	return (
-		<article css={wrapper}>
+		<article css={wrapper(content.products.length)}>
 			{content.header && <h1>{content.header}</h1>}
 			<ul>
-				{content.products.map((product) => (
-					<li key={product._id}>
-						{product.title} - {product.defaultPrice},-
+				{content.products.map((product, i) => (
+					<li
+						key={product._id}
+						css={css`
+							@media screen and (min-width: 600px) {
+								grid-area: ${`p${i + 1}`};
+							}
+						`}
+					>
+						<ProductPreview product={product} />
 					</li>
 				))}
 			</ul>
